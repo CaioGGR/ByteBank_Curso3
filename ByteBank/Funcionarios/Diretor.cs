fa2b9eb6b798e1547d759eb09868e28c8a9605d8
CaioGGR <caioggr@gmail.com>
2022-05-25 22:14:06 -0300

    Adicionado o Projeto do Curso 3 de C# da Alura (Até o momento: Cap 2)

---
+++ ByteBank/Funcionarios/Diretor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Diretor : Funcionario //Diretor é um funcionário
+    {
+
+        public override double GetBonificacao()
+        {
+            return salario + base.GetBonificacao(); // o base especifica que estamos falando do getbonificacao da class funcionario
+        }
+    }
+}
